3afe548ca3a802b5fe590e6ed4176a56b9c7d51e
David Thomas <dev@synth3.tk>
2020-08-01 01:53:36 -0400

    added support for teams and goals

--- extension.js
+++ extension.js
@@ -22,6 +22,15 @@ module.exports = function (nodecg) {
   var rewardsRep = nodecg.Replicant('rewards', {
     defaultValue: []
   })
+  var campaignGoalRep = nodecg.Replicant('goal', {
+    defaultValue: 0
+  })
+  var supportableRep = nodecg.Replicant('supportable', {
+    defaultValue: false
+  })
+  var supportingCampaignsRep = nodecg.Replicant('supportingcampaigns', {
+    defaultValue: []
+  })
 
   var TiltifyClient = require('tiltify-api-client')
 
@@ -37,23 +46,25 @@ module.exports = function (nodecg) {
 
   var client = new TiltifyClient(nodecg.bundleConfig.tiltify_api_key)
 
-  async function askTiltifyForDonations () {
-    client.Campaigns.getRecentDonations(nodecg.bundleConfig.tiltify_campaign_id, function (donations) {
-      for (let i = 0; i < donations.length; i++) {
-        var found = donationsRep.value.find(function (element) {
-          return element.id === donations[i].id
-        })
-        if (found === undefined) {
-          donations[i].shown = false
-          donations[i].read = false
-          donationsRep.value.push(donations[i])
+  async function askTiltifyForDonations (campaignId = nodecg.bundleConfig.tiltify_campaign_id) {
+    client.Campaigns.getRecentDonations(campaignId, function (donations) {
+      if (donations.length >= 1) {
+        for (let i = 0; i < donations.length; i++) {
+          var found = donationsRep.value.find(function (element) {
+            return element.id === donations[i].id
+          })
+          if (found === undefined) {
+            donations[i].shown = false
+            donations[i].read = false
+            donationsRep.value.push(donations[i])
+          }
         }
       }
     })
   }
 
-  async function askTiltifyForAllDonations () {
-    client.Campaigns.getDonations(nodecg.bundleConfig.tiltify_campaign_id, function (alldonations) {
+  async function askTiltifyForAllDonations (campaignId = nodecg.bundleConfig.tiltify_campaign_id) {
+    client.Campaigns.getDonations(campaignId, function (alldonations) {
       if (JSON.stringify(allDonationsRep.value) !== JSON.stringify(alldonations)) {
         allDonationsRep.value = alldonations
       }
@@ -94,29 +105,62 @@ module.exports = function (nodecg) {
 
   async function askTiltifyForTotal () {
     client.Campaigns.get(nodecg.bundleConfig.tiltify_campaign_id, function (campaign) {
-      if (campaignTotalRep.value !== parseFloat(campaign.amountRaised)) {
-        campaignTotalRep.value = parseFloat(campaign.amountRaised)
+      if (campaignTotalRep.value !== parseFloat(campaign.totalAmountRaised)) {
+        campaignTotalRep.value = parseFloat(campaign.totalAmountRaised)
+      }
+    })
+  }
+
+  async function askTiltifyForGoal () {
+    client.Campaigns.get(nodecg.bundleConfig.tiltify_campaign_id, function (campaign) {
+      if (campaignGoalRep.value !== parseFloat(campaign.fundraiserGoalAmount)) {
+        campaignGoalRep.value = parseFloat(campaign.fundraiserGoalAmount)
+      }
+    })
+  }
+
+  async function askTiltifyForCampaignSupportable () {
+    client.Campaigns.get(nodecg.bundleConfig.tiltify_campaign_id, function (campaign) {
+      if (supportableRep.value !== campaign.supportable) {
+        supportableRep.value = campaign.supportable
+      }
+    })
+  }
+
+  async function askTiltifyForSupportingCampaigns() {
+    client.Campaigns.getSupportingCampaigns(nodecg.bundleConfig.tiltify_campaign_id, function (supportingcampaigns) {
+      if (JSON.stringify(supportingCampaignsRep.value) !== JSON.stringify(supportingcampaigns)) {
+        supportingCampaignsRep.value = supportingcampaigns
       }
     })
   }
 
   function askTiltify () {
     askTiltifyForDonations()
+    if (supportableRep.value === true) {
+      for (const supportCampaign of supportingCampaignsRep.value) {
+        (async () => await askTiltifyForDonations(supportCampaign.id))();
+      }
+    }
     askTiltifyForPolls()
+    askTiltifyForGoal()
     askTiltifyForTotal()
     askTiltifyForChallenges()
     askTiltifyForSchedule()
     askTiltifyForRewards()
   }
 
+  askTiltifyForCampaignSupportable()
+  askTiltifyForSupportingCampaigns()
+
   setInterval(function () {
     askTiltify()
-  }, 5000)
+  }, 10000)
 
   setInterval(function() {
     askTiltifyForAllDonations()
   }, 10000)
 
-  askTiltify()
-  askTiltifyForAllDonations()
+  // askTiltify()
+  // askTiltifyForAllDonations()
 }
